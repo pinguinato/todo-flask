5ff4a7d7997bae36b039d6321f8811dbfe4bafa9
Roberto Gianotto <gianottoroberto@gmail.com>
2021-08-07 22:05:34 +0200

    creazione del database sqlite

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ Flask Tutorial - creazione di una app to do list
 
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) 
 
 
 @app.route('/')
@@ -17,4 +21,7 @@ def index():
 
 
 if __name__ == "__main__":
+    # creo il db
+    db.create_all()
+    # run dell'app
     app.run(debug=True)
