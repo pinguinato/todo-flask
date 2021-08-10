87f47b8ebbd7f9e017135e22790c7beab9798f0f
Roberto Gianotto <gianottoroberto@gmail.com>
2021-08-10 09:58:54 +0200

    Aggiunta delle funzioni update() e delete() per i task

--- app.py
+++ app.py
@@ -40,6 +40,24 @@ def add():
     return redirect(url_for("index"))
 
 
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # aggiorna un task
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # rimuove un task
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
 # questo mi permette di fare run dal Pycharm
 if __name__ == "__main__":
     # creo il db
